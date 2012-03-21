e550efcceb2feb636ee74b3b71bdfefa0caea4e3
Tom Willis <tom.willis@gmail.com>
2012-03-21 09:15:03 -0400

    remove pyramid as a dependency because the scaffold really only depends on pastescript until appengine decides they will distribute webob>1.1.1
    packaging bookkeeping for uploading to pypi

--- pyramid_appengine/scaffolds/__init__.py
+++ pyramid_appengine/scaffolds/__init__.py
@@ -1,21 +1,29 @@
 """
 scaffold
 """
-try: # pyramid 1.0.X
-    # "pyramid.paster.paste_script..." doesn't exist past 1.0.X
-    from pyramid.paster import paste_script_template_renderer
-    from pyramid.paster import PyramidTemplate
-except ImportError:
-    try: # pyramid 1.1.X, 1.2.X
-        # trying to import "paste_script_template_renderer" fails on 1.3.X
-        from pyramid.scaffolds import paste_script_template_renderer
-        from pyramid.scaffolds import PyramidTemplate
-    except ImportError: # pyramid >=1.3a2
-        paste_script_template_renderer = None
-        from pyramid.scaffolds import PyramidTemplate
+from paste.script.templates import Template
+from paste.util.template import paste_script_template_renderer
+import os
 
 
-class PyramidAppEngineStarterTemplate(PyramidTemplate):
+class PyramidAppEngineStarterTemplate(Template):
     _template_dir = "starter"
     summary = "Pyramid scaffold for appengine"
     template_renderer = staticmethod(paste_script_template_renderer)
+
+    # taken from pyramid/scaffolds.PyramidTemplate
+    def pre(self, command, output_dir, vars):
+        vars['random_string'] = os.urandom(20).encode('hex')
+        package_logger = vars['package']
+        if package_logger == 'root':
+            # Rename the app logger in the rare case a project is named 'root'
+            package_logger = 'app'
+        vars['package_logger'] = package_logger
+        return Template.pre(self, command, output_dir, vars)
+
+    def post(self, command, output_dir, vars):
+        self.out('Welcome to pyramid_appengine.  Go HACK!!!')
+        return Template.post(self, command, output_dir, vars)
+
+    def out(self, msg): # pragma: no cover (replaceable testing hook)
+        print msg
--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 from setuptools import setup, find_packages
+import os
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 version = "0.5"
-requires = ["pyramid<=1.2", "webob==1.1.1"]
+requires = ["pastescript"]
 setup(name='pyramid_appengine',
       version=version,
       description="Scaffold + Tools for creating/developing pyramid applications on Google App Engine",
-      long_description="""\
-""",
-      classifiers=[],
-      keywords='',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+          ],
+      keywords='web pyramid pylons google-app-engine',
       author='Tom Willis',
       author_email='tom@batterii.com',
-      url='',
+      url='https://github.com/twillis/pyramid_appengine',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
@@ -20,8 +31,6 @@ setup(name='pyramid_appengine',
       entry_points="""
       # -*- Entry points: -*-
       [paste.paster_create_template]
-      appengine_starter=pyramid_appengine.scaffolds:PyramidAppEngineStarterTemplate
-      [pyramid.scaffold]
       appengine_starter=pyramid_appengine.scaffolds:PyramidAppEngineStarterTemplate
       """,
       )
